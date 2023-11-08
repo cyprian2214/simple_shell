d33165cd25277685605f401b7690b8b5cdf9f8aa
Ndivhuwo Mashau <Cyprian2214@gmail.com>
2023-11-08 03:29:03 -0800

    Simple shell project by NC Mashau

--- Header.h
+++ Header.h
@@ -9,6 +9,50 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/**
+ * custom_strcmp - Compare two strings
+ * @str1: The first string
+ * @str2: The second string
+ * Return: 0 if equal, positive/negative if different
+ */
+int custom_strcmp(const char *str1, const char *str2);
+
+/**
+ * custom_signal_handler - Handle signals
+ * @signal: The signal to handle
+ */
+void custom_signal_handler(int signal);
+
+/**
+ * custom_strlen - Calculate the length of a string
+ * @str: The input string
+ * Return: The length of the string
+ */
+size_t custom_strlen(const char *str);
+
+/**
+ * custom_strdup - Duplicate a string
+ * @str: The string to duplicate
+ * Return: Pointer to the duplicated string
+ */
+char *custom_strdup(const char *str);
+
+/**
+ * custom_strcat - Concatenate two strings
+ * @dest: The destination string
+ * @src: The source string
+ * Return: Pointer to the concatenated string
+ */
+char *custom_strcat(char *dest, char *src);
+
+/**
+ * custom_strcpy - Copy one string to another
+ * @dest: The destination string
+ * @src: The source string
+ * Return: Pointer to the copied string
+ */
+char *custom_strcpy(char *dest, char *src);
+
 /* global variables */
 extern char **environ;
 #endif
--- shell.c
+++ shell.c
@@ -1,50 +1,5 @@
 #include "Header.h"
 #include <unistd.h>
-
-/**
- * custom_strcmp - Compare two strings
- * @str1: The first string
- * @str2: The second string
- * Return: 0 if equal, positive/negative if different
- */
-int custom_strcmp(const char *str1, const char *str2);
-
-/**
- * custom_signal_handler - Handle signals
- * @signal: The signal to handle
- */
-void custom_signal_handler(int signal);
-
-/**
- * custom_strlen - Calculate the length of a string
- * @str: The input string
- * Return: The length of the string
- */
-size_t custom_strlen(const char *str);
-
-/**
- * custom_strdup - Duplicate a string
- * @str: The string to duplicate
- * Return: Pointer to the duplicated string
- */
-char *custom_strdup(const char *str);
-
-/**
- * custom_strcat - Concatenate two strings
- * @dest: The destination string
- * @src: The source string
- * Return: Pointer to the concatenated string
- */
-char *custom_strcat(char *dest, char *src);
-
-/**
- * custom_strcpy - Copy one string to another
- * @dest: The destination string
- * @src: The source string
- * Return: Pointer to the copied string
- */
-char *custom_strcpy(char *dest, char *src);
-
 /**
  * custom_getline - Read a line from input stream
  * @line: A pointer to the line buffer
@@ -60,6 +15,7 @@ ssize_t custom_getline(char **line, size_t *len, FILE *stream);
  * @av: Argument vector
  * Return: Always 0
  */
+
 int main(int ac, char **av)
 {
 	/* User prompt */
